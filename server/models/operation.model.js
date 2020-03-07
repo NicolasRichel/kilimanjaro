d408055b7094870af03cb75cc6c1a54bb69146d5
NicolasRichel <nicolasrichel.github@gmail.com>
2020-03-07 07:16:18 +0100

    (server) : update label API

--- server/models/operation.model.js
+++ server/models/operation.model.js
@@ -4,9 +4,5 @@ module.exports = mongoose.model('Operation', new mongoose.Schema({
   date: { type: String, required: true },
   amount: { type: Number, required: true },
   reference: { type: String, required: true },
-  labels: [{
-    name: String,
-    color: String,
-    textColor: String
-  }]
+  labels: [String]
 }));
